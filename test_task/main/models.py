12592048d22ce0183a67566fb0640652f29f181d
EA2311 <andreyyerzhov@gmail.com>
2024-01-08 21:07:23 +0200

    feat: create Book model

--- test_task/main/models.py
+++ test_task/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    publish_year = models.IntegerField()
+
