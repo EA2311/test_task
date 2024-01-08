104f1141b21bfb7e2f63d297f38b334389274750
EA2311 <andreyyerzhov@gmail.com>
2024-01-08 21:10:26 +0200

    feat: create BookListView

--- test_task/main/views.py
+++ test_task/main/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from test_task.main.models import Book
+
+
+class BookListView(generic.ListView):
+    model = Book
